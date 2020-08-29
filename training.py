7c7b38babda157b476e9ac98f12750ccb174c756
Sidharth Srinath <sidharthsrinath@Sidharths-MacBook-Pro.local>
2020-08-29 15:17:19 -0700

    started neat implementation

--- training.py
+++ training.py
@@ -1,5 +1,7 @@
 from snake import *
 import pygame, sys, time, random
+import neat
+import os
 
 # DIFFICULTY settings
 # Easy      ->  10
@@ -49,29 +51,29 @@ def get_key_pressed(event):
         #     pygame.event.post(pygame.event.Event(pygame.QUIT))
     return key_pressed
 
-def game_over():
-    my_font = pygame.font.SysFont('times new roman', 90)
-    game_over_surface = my_font.render('YOU DIED', True, RED)
-    game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (FRAME_DIM[0]/2, FRAME_DIM[1]/4)
-    GAME_WINDOW.fill(BLACK)
-    GAME_WINDOW.blit(game_over_surface, game_over_rect)
-    show_score(0, RED, 'times', 20)
-    pygame.display.flip()
-    time.sleep(3)
-    pygame.quit()
-    sys.exit()
-
-def show_score(choice, color, font, size):
-    score_font = pygame.font.SysFont(font, size)
-    score_surface = score_font.render('Score : ' + str(score), True, color)
-    score_rect = score_surface.get_rect()
-    if choice == 1:
-        score_rect.midtop = (FRAME_DIM[0]/10, 15)
-    else:
-        score_rect.midtop = (FRAME_DIM[0]/2, FRAME_DIM[1]/1.25)
-    GAME_WINDOW.blit(score_surface, score_rect)
-    # pygame.display.flip()
+# def game_over():
+#     my_font = pygame.font.SysFont('times new roman', 90)
+#     game_over_surface = my_font.render('YOU DIED', True, RED)
+#     game_over_rect = game_over_surface.get_rect()
+#     game_over_rect.midtop = (FRAME_DIM[0]/2, FRAME_DIM[1]/4)
+#     GAME_WINDOW.fill(BLACK)
+#     GAME_WINDOW.blit(game_over_surface, game_over_rect)
+#     show_score(0, RED, 'times', 20)
+#     pygame.display.flip()
+#     time.sleep(3)
+#     pygame.quit()
+#     sys.exit()
+#
+# def show_score(choice, color, font, size):
+#     score_font = pygame.font.SysFont(font, size)
+#     score_surface = score_font.render('Score : ' + str(score), True, color)
+#     score_rect = score_surface.get_rect()
+#     if choice == 1:
+#         score_rect.midtop = (FRAME_DIM[0]/10, 15)
+#     else:
+#         score_rect.midtop = (FRAME_DIM[0]/2, FRAME_DIM[1]/1.25)
+#     GAME_WINDOW.blit(score_surface, score_rect)
+#     # pygame.display.flip()
 
 # Main
 def train():
@@ -156,8 +158,23 @@ def train():
         # Refresh rate
         fps_controller.tick(DIFFICULTY)
 
-def main():
-    train()
+
+def run(config_path):
+    # Upload the config file
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                    neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                    config_path)
+    # Create a population
+    pop = neat.Population(config)
+    # Print population
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    # Get the winning genome from the play function
+    winner = pop.run(train, 50)
+
 
 if __name__ == "__main__":
-    train()
+    local_directory = os.path.dirname(__file__)
+    config_path = os.path.join(local_directory, 'configuration.txt')
+    run(config_path)
